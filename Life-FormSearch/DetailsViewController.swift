491bbb826e895ddc80b40512be5ea63006e7a243
bumpagram <invoke@kxdigital.ru>
2024-02-26 16:53:13 +0300

    сделал аутлеты на все лейблы, дописал функцию updateUI. теперь всё по красоте: все поля заполняются, если не получили инфу от сервера - выводится API unavailable

--- Life-FormSearch/DetailsViewController.swift
+++ Life-FormSearch/DetailsViewController.swift
@@ -10,7 +10,7 @@ class DetailsViewController: UIViewController {
     var lifeForm: LifeForm  // // в это свойство пробросится элемент при переходе из SearchTVC по IBSegueAction
     var pagesResponse: PagesResponse?  // это получим из Task сетевого запроса с вложенными свойствами
     var hierarchyResponse: HierarchyResponse?
-
+    var correctItemInTaxonConcepts: TaxonConcepts?
     
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var detailsNavigationItem: UINavigationItem!
@@ -18,6 +18,12 @@ class DetailsViewController: UIViewController {
     @IBOutlet var licenseLabel: UILabel!
     @IBOutlet var nameAccordingToLabel: UILabel!
     @IBOutlet var scientificLabel: UILabel!
+    @IBOutlet var kingdomLabel: UILabel!
+    @IBOutlet var phylumLabel: UILabel!
+    @IBOutlet var classLabel: UILabel!
+    @IBOutlet var orderLabel: UILabel!
+    @IBOutlet var familyLabel: UILabel!
+    @IBOutlet var genusLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -35,6 +41,7 @@ class DetailsViewController: UIViewController {
         if pagesResponse != nil {
             scientificLabel.text = pagesResponse?.details.scientificName
             licenseLabel.text = pagesResponse?.details.dataObjects?.first?.license.formatted()  // форматировал URL в String для отображения в лейбле
+            nameAccordingToLabel.text = correctItemInTaxonConcepts?.nameAccordingTo
             if let existedRights = pagesResponse?.details.dataObjects?.first?.rightsHolder {
                 rightsHolderLabel.text = existedRights
             } else {
@@ -42,12 +49,37 @@ class DetailsViewController: UIViewController {
                 let role = pagesResponse?.details.dataObjects?.first?.agents.first?.role
                 rightsHolderLabel.text = "\(String(describing: fullname)), \(String(describing: role))"
             }
-            
         } else {
             scientificLabel.text = lifeForm.scientificName
+            licenseLabel.text = "API unavailable"
+            nameAccordingToLabel.text = "API unavailable"
         }
         
-        
+        if hierarchyResponse != nil {
+            kingdomLabel.text = "(No Data)"
+            phylumLabel.text = "(No Data)"
+            classLabel.text = "(No Data)"
+            orderLabel.text = "(No Data)"
+            familyLabel.text = "(No Data)"
+            genusLabel.text = "(No Data)"
+            
+            hierarchyResponse!.ancestors.forEach({ ancestor in
+                if ancestor.taxonRank?.lowercased() == "kingdom" { kingdomLabel.text = ancestor.scientificName }
+                if ancestor.taxonRank?.lowercased() == "phylum" { phylumLabel.text = ancestor.scientificName }
+                if ancestor.taxonRank?.lowercased() == "class" { classLabel.text = ancestor.scientificName }
+                if ancestor.taxonRank?.lowercased() == "order" { orderLabel.text = ancestor.scientificName }
+                if ancestor.taxonRank?.lowercased() == "family" { familyLabel.text = ancestor.scientificName }
+                if ancestor.taxonRank?.lowercased() == "genus" { genusLabel.text = ancestor.scientificName }
+            })
+            
+        } else {
+            kingdomLabel.text = "API unavailable"
+            phylumLabel.text = "API unavailable"
+            classLabel.text = "API unavailable"
+            orderLabel.text = "API unavailable"
+            familyLabel.text = "API unavailable"
+            genusLabel.text = "API unavailable"
+        }
     }
     
     
@@ -75,10 +107,12 @@ class DetailsViewController: UIViewController {
                             let someHierarchyResponse = try await NetworkClass.shared.fetchHierarchyAPI(for: item.identifier) // нужно передавать сюда identifier каждого элемента taxonConcepts. если проперти ancestors - не пустой массив, то стоп. и будем отображать его. если пустое- следующий элемент.
                             if !someHierarchyResponse.ancestors.isEmpty {
                                 self.hierarchyResponse = someHierarchyResponse
+                                self.correctItemInTaxonConcepts = item
                                 print("Successfully fetched HierarchyAPI")
                                 break
                             }
                         }
+                        updateUI()
                         
                     } catch {  print("Error fetching HierarchyAPI: \(error)")  }
                 }
